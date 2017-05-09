7baf3054d6907643159e10c19b039903351e5ddd
Erika <erika.rodrigues10@hotmail.com>
2017-05-09 14:16:01 -0300

    para migrar sem erro

--- database/migrations/2017_05_07_234648_Assunto.php
+++ database/migrations/2017_05_07_234648_Assunto.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-public class Assunto extends Migration
+class Assunto extends Migration
 {
     /**
      * Run the migrations.
