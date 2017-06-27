99c41d4a430d7d793cd8ac47fea332795ffbb6af
ErikaParhg <erika.rodrigues10@hotmail.com>
2017-06-27 20:51:36 -0300

    convites alterados

--- app/Http/Controllers/ReuniaoController.php
+++ app/Http/Controllers/ReuniaoController.php
@@ -10,21 +10,11 @@ use App\Mail\Convite;
 
 class ReuniaoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('home');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $items = DB::table('Assunto')->get();
@@ -37,12 +27,6 @@ class ReuniaoController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $id = auth()->id();
@@ -60,8 +44,7 @@ class ReuniaoController extends Controller
         $reuniao->Participantes = $request->participantes;
         $reuniao->save();
     }
-
-
+    
     public function testMail(Request $request)
     {
       $id = $request->participantes;                        
@@ -72,49 +55,5 @@ class ReuniaoController extends Controller
       Mail::to($mail)->send(new Convite($request));
 
       return redirect('/logado');
-    }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
