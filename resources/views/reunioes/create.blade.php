c1be41be6bcf1970d9e4e222499683379c78b0f8
MorganaCosta <mor.costa.s@gmail.com>
2017-06-28 16:50:30 -0300

    indentação 2

--- resources/views/reunioes/create.blade.php
+++ resources/views/reunioes/create.blade.php
@@ -1,123 +1,124 @@
 @extends('base')
 
 @section('content')
-
-<section class="content-header">
-    <h1>Reuniões
-        <small>Descrição</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="/admin"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Reuniões</li>
-    </ol>
-</section>
-@if (Route::has('login'))
-         @if (Auth::check())
-<section class="content">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Reuniões</h3>
-                    </div>
-                    <form  id="formReunioes" name="formReunioes" role="form" method="POST" action="/testMail">
-                     {{ csrf_field() }}
-                        <div class="box-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="assunto">Assunto</label>
-                                        <select id="assunto" name="assunto" class="form-control">
-                                            @foreach($itemlist as $items)
-                                            <option value="{{ $items }}">{{ $items }}</option>
-                                            @endforeach
-                                        </select>
-                                        <br><br>
-                                        <input type="checkbox" id="checkBox" /><label>Adicionar novo assunto: </label>
-                                        
-                                        <div id="addAssunto"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="tema">Tema</label>
-                                        <input type="tema" class="form-control required" id="tema" name="tema" placeholder="Tema">
-                                    </div>  
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="pautas">Pautas</label>
-                                        <input type="text" class="form-control required" id="pautas" name="pautas" placeholder="Pautas">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="descricao">Descrição</label>
-                                        <input type="text" class="form-control required" id="descricao" name="descricao" placeholder="Descrição">
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="data_hora">Data</label>
-                                        <input type="text" id="datepicker" name="datepicker" class="form-control required" placeholder="Data">
-                                    </div>
-                                </div>
-                               
-                                <div class="col-md-6">
-                                     <div class="form-group">
-                                        <label for="tipo_reuniao">Tipo de Reunião</label>
-                                        <select class="form-control" id="tipo_reuniao" name="tipo_reuniao">
-                                            <option value="reuniao_de_area">Reunião de Área</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="quorum">Quorum</label>
-                                        <br>
-                                        <input type="checkbox" id="clickQuorum" /><label>Adicionar Quorum? </label>
-                                        
-                                        <div id="addQuorum"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="segunda_chamada">Segunda Chamada?</label>
-                                        <br>
-                                        <input type="radio" id="segunda_chamada" name="segunda_chamada" value="1"> Sim
-                                        <input type="radio" id="segunda_chamada" name="segunda_chamada" value="0"> Não
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="participantes">Participantes</label>
-                                        <select multiple id="participantes" name="participantes" class="form-control">
-                                            @foreach($participa as $participantes)
-                                            <option value="{{ $participantes->id }}">{{ $participantes->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
+    <section class="content-header">
+        <h1>Reuniões
+            <small>Descrição</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="/admin"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Reuniões</li>
+        </ol>
+    </section>
+    @if (Route::has('login'))
+        @if (Auth::check())
+            <section class="content">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box box-primary">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Reuniões</h3>
                             </div>
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-primary">Salvar</button>
-                                <button class="btn btn-default back">Cancelar</button>
-                            </div>
+                            <form  id="formReunioes" name="formReunioes" role="form" method="POST" action="/testMail">
+                                {{ csrf_field() }}
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="assunto">Assunto</label>
+                                                <select id="assunto" name="assunto" class="form-control">
+                                                    @foreach($itemlist as $items)
+                                                    <option value="{{ $items }}">{{ $items }}</option>
+                                                    @endforeach
+                                                </select>
+                                                <br><br>
+                                                <input type="checkbox" id="checkBox" /><label>Adicionar novo assunto: </label>
+                                                
+                                                <div id="addAssunto"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="tema">Tema</label>
+                                                <input type="tema" class="form-control required" id="tema" name="tema" placeholder="Tema">
+                                            </div>  
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="pautas">Pautas</label>
+                                                <input type="text" class="form-control required" id="pautas" name="pautas" placeholder="Pautas">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="descricao">Descrição</label>
+                                                <input type="text" class="form-control required" id="descricao" name="descricao" placeholder="Descrição">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="data_hora">Data</label>
+                                                <input type="text" id="datepicker" name="datepicker" class="form-control required" placeholder="Data">
+                                            </div>
+                                        </div>
+                                       
+                                        <div class="col-md-6">
+                                             <div class="form-group">
+                                                <label for="tipo_reuniao">Tipo de Reunião</label>
+                                                <select class="form-control" id="tipo_reuniao" name="tipo_reuniao">
+                                                    <option value="reuniao_de_area">Reunião de Área</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="quorum">Quorum</label>
+                                                <br>
+                                                <input type="checkbox" id="clickQuorum" /><label>Adicionar Quorum? </label>
+                                                
+                                                <div id="addQuorum"></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="segunda_chamada">Segunda Chamada?</label>
+                                                <br>
+                                                <input type="radio" id="segunda_chamada" name="segunda_chamada" value="1"> Sim
+                                                <input type="radio" id="segunda_chamada" name="segunda_chamada" value="0"> Não
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="participantes">Participantes</label>
+                                                <select multiple id="participantes" name="participantes" class="form-control">
+                                                    @foreach($participa as $participantes)
+                                                    <option value="{{ $participantes->id }}">{{ $participantes->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-footer">
+                                        <button type="submit" class="btn btn-primary">Salvar</button>
+                                        <button class="btn btn-default back">Cancelar</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div></div></div>
-</section>
-     @else
-        <a href="{{ url('/login') }}">Login</a>
-        <a href="{{ url('/register') }}">Registrar</a>
+                    </div>
+                </div>
+            </section>
+        @else
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Registrar</a>
+        @endif
     @endif
-</div>
-@endif     
 @endsection
-<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous">
-</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+
 <script>
     $(function () {
         $("#clickQuorum").click(function () {
@@ -129,6 +130,7 @@
                 $('#addQuorum').find('#quorum').remove();
             }
         });
+        
         $("#checkBox").click(function () {
             $('#textBox').attr("disabled", $(this).is(":checked"));
             var $this = $(this);
@@ -137,7 +139,8 @@
             }else{
                 $('#addAssunto').find('#assunto').remove();
             }
-       });
+        });
+
         $('#icheck').iCheck();
         $('#datepicker').datepicker({
             format: 'dd/mm/yyyy',
@@ -172,8 +175,6 @@
                 participantes: $("#participantes").val(),
             }
 
-
-            
             $.ajax({
                 type: "POST",
                 url: "/reunioes",
